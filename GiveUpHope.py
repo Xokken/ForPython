4dc394a97d4dd95977af8b510e0c5cc0196b2813
Alexey Zam Zamich <57191485+Xokken@users.noreply.github.com>
2020-05-20 16:10:19 +0300

    Python and Java

---
+++ GiveUpHope.py
@@ -0,0 +1,3 @@
+print ("I am Alexey Dyakonov, as it may be known.")
+print ("I am a student at KFU, who wants to become a good programmer.")
+print ("I like to watch anime and put dots at the end of sentences.")
---
+++ ToEveryoneWhoReads.java
@@ -0,0 +1,7 @@
+public class ToEveryoneWhoReads {
+	public static void main(String[] args) {
+        System.out.println ("I am Alexey Dyakonov, as it may be known.");
+		System.out.println ("I am a student at KFU, who wants to become a good programmer.");
+		System.out.println ("I like to watch anime and put dots at the end of sentences.");
+    }
+}
